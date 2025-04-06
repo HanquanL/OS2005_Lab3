16942ad87faee6a1913da7b33ae6b90ed1c1f606
HanquanL <Hanquanl0115@gmail.com>
2025-04-06 19:55:09 -0400

    add process, vma, and page_table

--- mmu.h
+++ mmu.h
@@ -5,10 +5,39 @@
 
 using namespace std;
 
+#define MAX_PTE 64
+#define MAX_FRAMES 128
 
+class Process{
+    public:
+        vector<vma> vmas;
+        page_table page_table[MAX_PTE];
 
+};
 
+class vma{
+    public:
+        int start_vpage;
+        int end_vpage;
+        int write_protected;
+        int file_mapped;
+        vma(int start_vpage, int end_vpage, int write_protected, int file_mapped){
+            this->start_vpage = start_vpage;
+            this->end_vpage = end_vpage;
+            this->write_protected = write_protected;
+            this->file_mapped = file_mapped;
+        }
+};
 
+struct page_table{
+    unsigned int PRESENT: 1; 
+    unsigned int REFERENCED: 1;
+    unsigned int MODIFIED: 1;
+    unsigned int WRITE_PROTECTED: 1;
+    unsigned int PAGEDOUT: 1;
+    unsigned int page_frame_number: 7;
+    unsigned int free_to_use: 20;
+};
 
 
 
